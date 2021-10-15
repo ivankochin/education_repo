16bdef96bb65049e75b8259e683f838744dcb43a
Kochin Ivan <kochin.ivan@intel.com>
2021-10-15 22:20:38 +0300

    Implement std::atomic<std::shared_ptr<T>> based version

--- cpp/patterns_and_practics/concurrency_in_action/lock_free_stack.cpp
+++ cpp/patterns_and_practics/concurrency_in_action/lock_free_stack.cpp
@@ -1,23 +1,43 @@
 #include <utility>
 #include <atomic>
+#include <memory>
+#include <iostream>
+
+// TODO: relax the fences where possible
+// TODO: Implement the version with two reference counters (internal/external)
 
 template <typename T>
 struct lock_free_stack {
 private:
     struct node {
         T data;
-        node* next;
+        std::shared_ptr<node> next;
     };
 
-    std:atomic<node*> head{nullptr};
+    // Requires C++20
+    std::atomic<std::shared_ptr<node>> head{};
 
+public:
+    lock_free_stack() = default;
     lock_free_stack(const lock_free_stack& other) = delete;
     lock_free_stack& operator=(const lock_free_stack& other) = delete;
 
     void push(T elem) {
-        node* new_head = new node{std::move(elem), head.load()};
+        std::shared_ptr<node> new_head = std::make_shared<node>(std::move(elem), head.load());
         while(!head.compare_exchange_weak(new_head->next, new_head)) {}
     }
 
-    T pop(); // TODO: implement
+    T pop() {
+        std::shared_ptr<node> old_head = head.load();
+        while(old_head.get() && !head.compare_exchange_weak(old_head, old_head->next)) {}
+
+        return old_head->data;
+    }
 };
+
+int main() {
+    lock_free_stack<int> int_stack;
+    int_stack.push(1);
+
+    std::cout << int_stack.pop() << std::endl;
+}
