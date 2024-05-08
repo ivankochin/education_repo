b9ae5b110f6a641eb7779d92ad4de3fa21e0e0f1
Ivan Kochin <iv.kochin@gmail.com>
2024-05-08 10:22:11 +0400

    Initial predict implementation

--- ml/mnist_model/helpers/print_png.h
+++ ml/mnist_model/helpers/print_png.h
@@ -4,59 +4,70 @@
 
 #include <png.h>
 
-#define IMAGE_HEIGHT 250
-#define IMAGE_WIDTH 300
-#define IMAGE_GRAY 50
-
 #define PNG_SETJMP_NOT_SUPPORTED 1
 
-using line_type = std::vector<png_byte>;
-using image_type = std::vector<line_type>;
+namespace png {
 
-image_type make_image(std::size_t height, std::size_t width) {
-    return image_type(height, line_type(width));
+    namespace {
+    void close_file(std::FILE* fp) {
+        std::fclose(fp);
+    }
+    }
+
+class image {
+    std::vector<png_byte> data;
+    std::size_t height;
+    std::size_t width;
+public:
+    image(std::size_t input_height, std::size_t input_width)
+        : data(input_height * input_width)
+        , height{input_height}
+        , width{input_width}
+    {}
+
+    void set_pixel(std::size_t x, std::size_t y, png_byte value) {
+        data[x + y * width] = value;
+    }
+
+    const std::vector<png_byte>& get_data() {
+        return data;
+    }
+
+    void write_to_file(const char* filename)
+    {
+        std::vector<png_byte*> write_image_view(height);
+        std::unique_ptr<std::FILE, decltype(&close_file)> fp(std::fopen(filename, "wb"), &close_file);
+
+        for (int i = 0; i < height; ++i) {
+            write_image_view[i] = data.data() + i * width;
+        }
+
+        // Create struct for write
+        png_struct* png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        if (!png) {
+            printf("Create write struct failed\n");
+            return;
+        }
+        // Create struct for info
+        png_infop info = png_create_info_struct(png);
+        if (!info) {
+            return;
+        }
+
+        // Set file output
+        png_init_io(png, fp.get());
+        // Set image properties
+        png_set_IHDR(png, info, height, width, 8, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE,
+                     PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+        // Write png image information to file
+        png_write_info(png, info);
+        // Write image data
+        png_write_image(png, write_image_view.data());
+        png_write_end(png, NULL);
+    }
+};
+
+image make_image(std::size_t height, std::size_t width) {
+    return image(height, width);
 }
-
-void close_file(std::FILE* fp)
-{
-    std::fclose(fp);
-}
-
-void write_png_image(image_type& image, const char* filename)
-{
-    // std::vector<line_type> image(IMAGE_HEIGHT, line_type(IMAGE_WIDTH));
-    std::vector<png_byte*> write_image_view(image.size());
-    std::unique_ptr<std::FILE, decltype(&close_file)> fp(std::fopen(filename, "wb"), &close_file);
-
-    for (int i = 0; i < write_image_view.size(); ++i) {
-        write_image_view[i] = image[i].data();
-    }
-    // for (line_type& line: image) {
-    //     for (png_byte& point: line) {
-    //         point = IMAGE_GRAY;
-    //     }
-    // }
-
-    // Create struct for write
-    png_struct* png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png) {
-        printf("Create write struct failed\n");
-        return;
-    }
-    // Create struct for info
-    png_infop info = png_create_info_struct(png);
-    if (!info) {
-        return;
-    }
-
-    // Set file output
-    png_init_io(png, fp.get());
-    // Set image properties
-    png_set_IHDR(png, info, image.size(), image.front().size(), 8, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE,
-                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-    // Write png image information to file
-    png_write_info(png, info);
-    // Write image data
-    png_write_image(png, write_image_view.data());
-    png_write_end(png, NULL);
 }
--- ml/mnist_model/mnist_model.cpp
+++ ml/mnist_model/mnist_model.cpp
@@ -1,8 +1,142 @@
+// g++ mnist_model.cpp -lpng -g
+
 #include "helpers/print_png.h"
 #include "helpers/rapidcsv.h"
 
+
+// uncomment to disable assert()
+// #define NDEBUG
+#include <cassert>
+ 
+// Use (void) to silence unused warnings.
+#define assertm(exp, msg) assert(((void)msg, exp))
+
+
+#include <algorithm>
+#include <random>
+#include <list>
+
+
 constexpr std::size_t image_height = 28;
 constexpr std::size_t image_width  = 28;
+constexpr std::size_t pixels_count = image_height * image_width;
+
+
+namespace ml {
+using node = std::vector<double>;
+
+class layer {
+public:
+    using result = std::vector<double>;
+    using activation_function = void (*)(result&);
+    using initialization_function = void(*)(node&);
+private:
+    std::vector<node> data;
+    std::size_t weights_count;
+    activation_function act_func;
+    initialization_function init_func;
+public:
+    layer(std::size_t nodes_count, std::size_t input_weights_count, activation_function input_act_func,
+          initialization_function input_init_func)
+        : data(nodes_count, node(input_weights_count + 1))
+        , weights_count(input_weights_count)
+        , act_func(input_act_func)
+        , init_func(input_init_func)
+    {
+        for (auto& n: data) {
+            init_func(n);
+        }
+    }
+
+    std::size_t nodes_count() const {
+        return data.size();
+    }
+
+    template <typename Input>
+    result process(const Input& input) const {
+        assertm(input.size() == weights_count, "Wrong input size");
+
+        result res(data.size());
+        for (int i = 0; i < data.size(); ++i) {
+            int j = 0;
+            for(; j < weights_count; ++j) {
+                res[i] += data[i][j] * input[j];
+            }
+            res[i] += data[i][j]; // add bias
+        }
+
+        act_func(res);
+
+        return res;
+    }
+};
+
+
+namespace act {
+    void relu(layer::result& input) {
+        for (auto& el: input) {
+            el = std::max(0.0, el);
+        }
+    }
+
+    void soft_max(layer::result& input) {
+        auto sum = std::accumulate(std::begin(input), std::end(input), 0);
+        for (auto& el: input) {
+            el = el / sum;
+        }
+    }
+}
+
+namespace init {
+    void random(node& input_node) {
+        static std::uniform_real_distribution<double> unif(-1, 1);
+        static std::default_random_engine re;
+
+        for(auto& weight: input_node) {
+            weight = unif(re);
+        }
+    }
+
+    void zero(node& input_node) {
+        for(auto& weight: input_node) {
+            weight = 0;
+        }
+    }
+}
+
+class model {
+    std::list<layer> layers{};
+    layer::initialization_function init_func;
+public:
+
+    model(std::size_t input_size, std::size_t nodes_count, layer::activation_function act_func,
+          layer::initialization_function input_init_func)
+        : layers{}
+        , init_func(input_init_func)
+    {
+        layers.emplace_back(nodes_count, input_size, act_func, init_func);
+    }
+
+    void add_next_layer(std::size_t nodes_count, layer::activation_function act_func) {
+        std::size_t weights_count = layers.back().nodes_count();
+
+        layers.emplace_back(nodes_count, weights_count, act_func, init_func);
+    }
+
+    template <typename Input>
+    layer::result predict(const Input& input) {
+        auto layer_it = std::begin(layers);
+        layer::result processing_data = layer_it->process(input);
+        ++layer_it;
+
+        for (; layer_it != std::end(layers); ++layer_it) {
+            processing_data = layer_it->process(processing_data);
+        }
+
+        return processing_data;
+    }
+};
+}
 
 int main() {
     rapidcsv::Document doc("data/mnist_train.csv");
@@ -11,11 +145,20 @@ int main() {
     int label = *image_it++;
     std::cout << "label is " << label << std::endl;
 
-    image_type image = make_image(image_height, image_width);
-    for (int i = 0; i < image_height; ++i) {
-        for (int j = 0; j < image_width; ++j) {
-            image[i][j] = *image_it++;
+    png::image image = png::make_image(image_height, image_width);
+    for (int y = 0; y < image_height; ++y) {
+        for (int x = 0; x < image_width; ++x) {
+            image.set_pixel(x, y, *image_it++);
         }
     }
-    write_png_image(image, "test_file.png");
+    image.write_to_file("image.png");
+
+    ml::model mnist_model(pixels_count, 16, ml::act::relu, ml::init::random);
+    mnist_model.add_next_layer(16, ml::act::relu);
+    mnist_model.add_next_layer(10, ml::act::soft_max);
+
+    auto result = mnist_model.predict(image.get_data());
+    for (std::size_t i = 0; i < result.size(); ++i) {
+        std::cout << " i = " << i << " probability = " << result[i] << std::endl;
+    }
 }
